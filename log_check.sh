45a10372c6bbd687304549887ea79f61274afa8d
vsvijesh <45142529+vsvijesh@users.noreply.github.com>
2019-03-26 12:32:28 +0530

    Create log_check.sh

---
+++ log_check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+####################################################
+# Check the app log for the occurance of <PATTERN> #
+# and throw error if not found within <THRESHOLD>  #
+#                                                  #
+####################################################
