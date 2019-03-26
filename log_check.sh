a90577714fd2d303451aeb0916fe2f3c608ff206
Vijesh V S <vsvijesh@gmail.com>
2019-03-26 18:42:14 +0530

    modified log_check

--- log_check.sh
+++ log_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ####################################################
-# Check the app log for the occurance of <PATTERN> #
-# and throw error if not found within <THRESHOLD>  #
+# Continuously check the app log for the occurance #
+# of <PATTERN> and throw error if not found within #
+# <THRESHOLD>                                      #
 #                                                  #
 ####################################################
